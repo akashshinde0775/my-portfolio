6395a0f3f038fb2b37ee2501b1671f863f649ecd
akashshinde0775 <akashshinde0775@gmail.com>
2025-01-31 21:12:07 +0530

    changes made

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/akashshinde0775/", // ✅ Set this to your repo name
+});
