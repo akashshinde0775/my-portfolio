19bf4989e90af14081a23770f5e6f2ec8b83db3f
akashshinde0775 <akashshinde0775@gmail.com>
2025-02-01 10:59:43 +0530

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/akashshinde0775/", // ✅ Set this to your repo name
+  base: "/my-portfolio/", // ✅ Set this to your repo name
 });
